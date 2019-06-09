9b3b5b45ece59814fb8f5f3bcc9310b176bcd3de
ehunternewton@gmail.com <ehunternewton@gmail.com>
2019-06-09 16:35:36 -0500

    created calendar for 6/2019

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import calendar
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Create Calendar
+    c = calendar.TextCalendar(calendar.SUNDAY)
+    month = c.monthdatescalendar(2019, 6)
+    return render_template('index.html', month=month)
 
 
 if __name__ == '__main__':
