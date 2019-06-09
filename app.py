fd83a13a16731e77c6f36f6464fce4fa43931827
ehunternewton@gmail.com <ehunternewton@gmail.com>
2019-06-09 17:27:26 -0500

    create month calender for current month and added styling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import calendar
+import datetime
 
 app = Flask(__name__)
 
@@ -7,9 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Create Calendar
+    today = datetime.date.today()
     c = calendar.TextCalendar(calendar.SUNDAY)
-    month = c.monthdatescalendar(2019, 6)
-    return render_template('index.html', month=month)
+    month = c.monthdatescalendar(today.year, today.month)
+    return render_template('index.html', month=month, today=today)
 
 
 if __name__ == '__main__':
